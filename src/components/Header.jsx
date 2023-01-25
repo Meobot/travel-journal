9a62f6ee4438b0d3e882c045b94fb5459113a62a
Meobot <nathanrhodes312@gmail.com>
2023-01-25 16:02:45 -0500

    debugging

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <div className="header">
-            <img className="header--globe" src="/globe.png" alt="image of a globe" />
+            <img className="header--globe" src="globe.png" alt="image of a globe" />
             <h1 className="header--text">my travel journal.</h1>
         </div>
     )
